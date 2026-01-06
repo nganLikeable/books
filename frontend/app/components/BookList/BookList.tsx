e0c4bfd6c190e331ca898b688fc020c60aac19b8
Ngan <ngannguyen141123@gmail.com>
2026-01-06 13:15:31 +1100

    restyled searchPage and booklist with tailwind

--- frontend/app/components/BookList/BookList.tsx
+++ frontend/app/components/BookList/BookList.tsx
@@ -1,9 +1,12 @@
 import { BookListProps } from "@/app/types/book";
 import BookCard from "../BookCard/BookCard";
-import styles from "./BookList.module.css";
 export default function BookList(prop: BookListProps) {
   const bookItems = prop.books.map((book) => (
     <BookCard key={book.key} book={book} />
   ));
-  return <div className={styles.grid}>{bookItems}</div>;
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-7.5 my-16 mx-auto px-20 w-full">
+      {bookItems}
+    </div>
+  );
 }
