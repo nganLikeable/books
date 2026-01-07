10916a15f3651787e3d82332649446934ef5b7aa
Ngan <ngannguyen141123@gmail.com>
2026-01-07 11:31:35 +1100

    added useAuthor hook to fetch author

---
+++ frontend/app/hooks/useAuthor.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export function useAuthor(id: string) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    if (id) {
+      return;
+    }
+    const getAuthor = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/author/${id}`);
+        if (!res.ok) {
+          return;
+        }
+        const dataRes = await res.json();
+        setData(dataRes);
+      } catch (e) {
+        console.log("error fetching author details");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getAuthor();
+  }, [id]);
+  return { data, loading };
+}
