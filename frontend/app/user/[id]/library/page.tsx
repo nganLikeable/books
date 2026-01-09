f63435eb8648c304350e806aa2ec567475e7e29c
Ngan <ngannguyen141123@gmail.com>
2026-01-09 23:50:19 +1100

    added info to display on dashboard

--- frontend/app/user/[id]/library/page.tsx
+++ frontend/app/user/[id]/library/page.tsx
@@ -2,6 +2,7 @@
 
 import Spinner from "@/app/components/Spinner/Spinner";
 import useFetchUserBooks from "@/app/hooks/useFetchUserBooks";
+import Link from "next/link";
 export default function Library() {
   const { books, loading } = useFetchUserBooks();
 
@@ -24,4 +25,47 @@ export default function Library() {
   const read = books.filter((book: any) => book.status === "READ");
 
   console.log("WANT: ", wantToRead, "READING:", reading, "READ:", read);
+
+  // create dictionary for looping
+  const sections = [
+    { title: "Want to read", data: wantToRead },
+    { title: "Currently reading", data: reading },
+    { title: "Read", data: read },
+  ];
+
+  return (
+    <div>
+      {sections.map((s, sIndex) => (
+        <div key={`${s.title}-${Date.now()}`}>
+          <div>
+            <h2>{s.title}</h2>
+            <span>Count: {s.data.length}</span>
+          </div>
+          {s.data.length === 0 ? (
+            <p>No books</p>
+          ) : (
+            <div>
+              {s.data.map((b: any) => {
+                const book = b.book ?? b; // support either nested book or flat
+                const authors = book?.authors || [];
+                console.log(authors);
+                return (
+                  <div key={book.id}>
+                    <Link href={`/book/${book.id}`}>{book.title}</Link>
+                    {authors.map((a: any, aIndex: number) => {
+                      return (
+                        <div key={`${a.id}`}>
+                          <Link href={`/author/${a.id}`}>{a.name}</Link>
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
