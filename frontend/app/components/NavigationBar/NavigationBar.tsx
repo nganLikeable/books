7e30c6da3fa0a0efecd69de091b7359f6760bd09
Ngan <ngannguyen141123@gmail.com>
2026-01-09 12:09:03 +1100

    added skeleton packages, changed logo style

--- frontend/app/components/NavigationBar/NavigationBar.tsx
+++ frontend/app/components/NavigationBar/NavigationBar.tsx
@@ -9,10 +9,14 @@ export default function NavBar() {
   const [user, loading, error] = useAuthState(auth);
 
   return (
-    <nav className="w-full sticky top-0 bg-background text-foreground border-b border-gray-200 dark:border-gray-800 shadow-sm z-50">
+    <nav className="w-full sticky top-0 bg-background text-foreground border-b border-gray-200 dark:border-gray-800 shadow-sm z-50 p-1">
       <div className="flex flex-row justify-around items-center  ">
-        <Link href="/" className="cursor-pointer">
-          <img src="/logo_yel.png" alt="NganBooks logo" className="w-40 " />
+        <Link
+          href="/"
+          className="text-2xl font-black tracking-tighter cursor-pointer bg-linear-to-r from-amber-500 to-orange-600 bg-clip-text text-transparent transition-all duration-300 group-hover:from-orange-500 group-hover:to-amber-600"
+        >
+          Bookie
+          {/* <img src="/logo_yel.png" alt="NganBooks logo" className="w-40 " /> */}
         </Link>
 
         <div className="flex flex-row justify-around gap-6 items-center ">
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,17 @@
 export default function App() {
   return (
     <div className="max-h-screen flex flex-col items-center justify-center p-10 ">
-      <div className=" w-full flex flex-col items-center gap-8">
+      <div className=" w-full flex flex-col items-center gap-8 text-5xl">
         <img
           className="w-full max-w-sm rounded-2xl shadow-2xl"
           src="/home.png"
           alt="home page photo"
         />
-        <h1 className="text-5xl md:text-6xl font-bold text-center">
-          Welcome to <span className="text-amber-600">Bookie</span>
+        <h1 className=" md:text-6xl font-bold text-center">
+          Welcome to{" "}
+          <span className=" font-black tracking-tighter  bg-linear-to-r from-amber-500 to-orange-600 bg-clip-text text-transparent ">
+            Bookie
+          </span>
         </h1>
         <p className="text-lg md:text-xl text-slate-600 text-center max-w-2xl">
           Discover, track, and organize your reading journey
