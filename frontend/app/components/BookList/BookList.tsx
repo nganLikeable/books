03ed98215740b7e7a6d762d86155493651f1b57c
Ngan <ngannguyen141123@gmail.com>
2026-01-12 11:28:13 +1100

    fixed unique key prop in booklist, replaced author with new type from types/db in shelfmodal

--- frontend/app/components/BookList/BookList.tsx
+++ frontend/app/components/BookList/BookList.tsx
@@ -4,7 +4,9 @@ export default function BookList({ books }: { books: Book[] }) {
   //  filter out undefined or null objs
   const bookItems = books
     .filter(Boolean)
-    .map((book) => <BookCard key={book.id} book={book} />);
+    .map((book: Book, index: number) => (
+      <BookCard key={`${book.id}-${index}`} book={book} />
+    ));
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-7.5  w-full">
       {bookItems}
