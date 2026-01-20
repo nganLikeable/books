b13277a6f606e3c435d0fa9f1ebf6096cc1bc508
Ngan <ngannguyen141123@gmail.com>
2026-01-20 20:07:08 +1100

    added proxy

---
+++ frontend/proxy.ts
@@ -0,0 +1,19 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export default function proxy(request: NextRequest) {
+  // get active token - cookie
+  const token = request.cookies.get("auth-token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  // no redirect or blocking
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/user/:userId([a-zA-Z0-9]+)/library",
+};
