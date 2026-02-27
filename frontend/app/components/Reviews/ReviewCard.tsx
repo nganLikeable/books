bf648c2f412992d0fd9338305c6f7d460396f7d5
Ngan <ngannguyen141123@gmail.com>
2026-02-28 01:02:22 +0700

    added loading, styled review card

--- frontend/app/components/Reviews/ReviewCard.tsx
+++ frontend/app/components/Reviews/ReviewCard.tsx
@@ -1,15 +1,32 @@
 import { Review } from "@/app/types/database";
+import Link from "next/link";
 import Rating from "../StarRating/Rating";
 
 export default function ReviewCard({ review }: { review: Review }) {
   const book = review?.book;
 
   return (
-    <div>
-      <h1>{book.title}</h1>
-      <img src={book?.cover || ""}></img>
-      <Rating no={review.rating} />
-      <p>{review.content}</p>
+    <div className="flex gap-4 p-4 bg-white rounded-2xl border border-gray-100 shadow-sm hover:shadow-md transition-all">
+      <div className="w-16 h-24 flex-shrink:0 bg-gray-50 rounded-lg overflow-hidden border border-gray-100 shadow-inner">
+        <img src={book?.cover}></img>
+      </div>
+
+      <div className="flex flex-col flex-1 min-w-0">
+        <h1 className="font-bold text-sm text-foreground truncate mb-1 leading-tight">
+          <Link href={`/book/${review.bookId}`}> {book.title}</Link>
+        </h1>
+        <div className="mb-2">
+          <Rating no={review.rating} />
+        </div>
+        <span className="text-[10px] text-gray-400 font-medium mb-3">
+          Last Updated: {new Date(review.updatedAt).toLocaleDateString()}
+        </span>
+        {review.content && (
+          <p className="text-xs text-gray-600 line-clamp-2 italic leading-relaxed">
+            {review.content}
+          </p>
+        )}{" "}
+      </div>
     </div>
   );
 }
--- frontend/app/user/[id]/reviews/page.tsx
+++ frontend/app/user/[id]/reviews/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 import ReviewCard from "@/app/components/Reviews/ReviewCard";
+import Spinner from "@/app/components/Spinner/Spinner";
 import useFetchReviews from "@/app/hooks/useFetchReviews";
 import { Review } from "@/app/types/database";
 
 export default function Reviews() {
-  const { reviews } = useFetchReviews();
-
+  const { reviews, loading } = useFetchReviews();
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-[80vh]">
+        <Spinner />
+      </div>
+    );
+  }
   return (
-    <div>
+    <div className="max-w-6xl mx-auto p- md:p-10">
       {reviews
         .filter((r) => r.content && r.rating)
         .map((r: Review) => (
-          <div key={r.id}>
+          <div key={r.id} className="grid grid-cols-1">
             <ReviewCard review={r} />
           </div>
         ))}
