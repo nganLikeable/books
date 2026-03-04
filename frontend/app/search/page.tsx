d22474d5575392fda0cbed5c1a5ed439a44539ed
Ngan <ngannguyen141123@gmail.com>
2026-03-04 22:18:32 +1100

    wrapped in suspense

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import SearchPage from "../components/SearchPage/SearchPage";
 
 export default function Search() {
-  return <SearchPage />;
+  return (
+    <Suspense fallback={<div className="my-16 mx-auto px-20">Loading...</div>}>
+      <SearchPage />
+    </Suspense>
+  );
 }
